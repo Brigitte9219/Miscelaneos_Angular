ccc59d204215fa92af3f50aca2c755a07a9d651a
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-26 14:11:30 -0500

    Rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle/usuario-detalle.component';
 
 const routes: Routes = [
-  {path:'home', component:HomeComponent},
-  {path:'usuario', component:UsuarioComponent},
+  {path:'home', component: HomeComponent},
+  {path:'usuario/:id',
+    component: UsuarioComponent,
+    children:[
+      {path:'nuevo', component: UsuarioNuevoComponent},
+      {path:'editar', component: UsuarioEditarComponent},
+      {path:'detalle', component: UsuarioDetalleComponent},
+      {path:'**', pathMatch:'full', redirectTo:'nuevo'}
+]},
   {path:'**', pathMatch:'full', redirectTo:'home'}
 ];
 
