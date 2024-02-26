4816468f143d004d8ae0a41cf23710996f7b9e8c
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-26 11:12:32 -0500

    Aplicando CSS a un solo componente

---
+++ src/app/components/css/css.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-css',
+  template:`
+  <p> css works!</p>`,
+  styles:[`
+  p{
+    color:red;
+  font-size:20px;
+  }
+  `]
+})
+export class CssComponent {
+
+}
