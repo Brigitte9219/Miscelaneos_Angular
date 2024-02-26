26233f8ff06314412c8461824ad13b990ea1e4d5
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-26 14:21:53 -0500

    Separar las rutas hijas a un archivo específico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,11 @@ import { UsuarioComponent } from './components/usuario/usuario.component';
 import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo/usuario-nuevo.component';
 import { UsuarioEditarComponent } from './components/usuario/usuario-editar/usuario-editar.component';
 import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle/usuario-detalle.component';
+import { usuario_routes } from './components/usuario/usuario.routes';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
-  {path:'usuario/:id',
-    component: UsuarioComponent,
-    children:[
-      {path:'nuevo', component: UsuarioNuevoComponent},
-      {path:'editar', component: UsuarioEditarComponent},
-      {path:'detalle', component: UsuarioDetalleComponent},
-      {path:'**', pathMatch:'full', redirectTo:'nuevo'}
-]},
+  {path:'usuario/:id', component: UsuarioComponent, children: usuario_routes},
   {path:'**', pathMatch:'full', redirectTo:'home'}
 ];
 
